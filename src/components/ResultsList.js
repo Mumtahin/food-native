7a8e9f34cdc75cd33c17d89d2ee10ae7a377ec08
Mumtahin <n.mumtahin1@gmail.com>
2020-12-28 23:14:42 +0000

    add styling to results list

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -4,11 +4,11 @@ import ResultsDetail from './ResultsDetail';
 
 const ResultsList = ({ title, results }) => {
 	return (
-		<View>
+		<View style={styles.container}>
 			<Text style={styles.title}>{title}</Text>
-			<Text>Results: {results.length}</Text>
 			<FlatList
 				horizontal
+				showsHorizontalScrollIndicator={false}
 				data={results}
 				keyExtractor={(results) => results.id}
 				renderItem={({item}) => {
@@ -23,7 +23,11 @@ const styles = StyleSheet.create({
 	title: {
 		fontSize: 18,
 		fontWeight: 'bold',
-		marginLeft: 15
+		marginLeft: 15,
+		marginBottom: 5
+	},
+	container: {
+		marginBottom: 10
 	}
 });
 
